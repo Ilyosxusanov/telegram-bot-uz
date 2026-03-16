7e5bc0810484d7f1d220ec82f93b0b0865ff1108
Xusanov Ilyosjon <129728100+Ilyosxusanov@users.noreply.github.com>
2026-03-16 07:05:52 +0500

    Update utils/states.py via API

---
+++ utils/states.py
@@ -0,0 +1,20 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class AdminStates(StatesGroup):
+    waiting_for_reply = State()
+    target_user_id = State()
+
+class AddMovieStates(StatesGroup):
+    category = State()
+    title = State()
+    media = State()
+    imdb = State()
+    code = State()
+
+class EditMovieStates(StatesGroup):
+    waiting_for_code = State()
+    choosing_field = State()
+    updating_value = State()
+
+class AIStates(StatesGroup):
+    chatting = State()
