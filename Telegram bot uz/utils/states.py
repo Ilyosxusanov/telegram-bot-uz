9753d52157142a7a856b75b50c8d3d76c5112c1f
Xusanov Ilyosjon <129728100+Ilyosxusanov@users.noreply.github.com>
2026-03-16 07:13:57 +0500

    Update Telegram bot uz/utils/states.py via API

--- Telegram bot uz/utils/states.py
+++ Telegram bot uz/utils/states.py
@@ -1,21 +1,20 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class AdminStates(StatesGroup):
-        waiting_for_reply = State()
-        target_user_id = State()
+    waiting_for_reply = State()
+    target_user_id = State()
 
 class AddMovieStates(StatesGroup):
-        category = State()
-        title = State()
-        media = State()
-        imdb = State()
-        code = State()
+    category = State()
+    title = State()
+    media = State()
+    imdb = State()
+    code = State()
 
 class EditMovieStates(StatesGroup):
-        waiting_for_code = State()
-        choosing_field = State()
-        updating_value = State()
+    waiting_for_code = State()
+    choosing_field = State()
+    updating_value = State()
 
 class AIStates(StatesGroup):
-        chatting = State()
-    
+    chatting = State()
